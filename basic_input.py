ed5141c3a8342b594692af8e228b7e206cd785e7
GongJae <73451911+whdudwo0428@users.noreply.github.com>
2024-09-09 12:01:16 +0900

    Add files via upload

---
+++ basic_input.py
@@ -0,0 +1,15 @@
+# 백준 a+b
+'''
+a, b = map(int, input().split())
+print(a+b)
+
+lst = list(map(int, input().split()))
+print(sum(lst))
+'''
+#print(sum(list(map(int, input().split()))))
+
+# 백준 ??!
+#print(input()+"??!")
+
+# 한줄롱
+print(int(input())-543)
